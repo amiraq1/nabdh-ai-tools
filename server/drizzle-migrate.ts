3fedf62eedae74bb685e69c7efd9ba4d99249441
amiraq1 <amaralmdarking27@gmail.com>
2025-12-06 19:50:34 +0300

    Update drizzle-migrate.ts

--- server/drizzle-migrate.ts
+++ server/drizzle-migrate.ts
@@ -1,15 +1,16 @@
 // server/drizzle-migrate.ts
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 import { db, pool } from "./db";
+import { logger } from "./logger";
 
 async function main() {
-  console.log("Running drizzle migrations...");
+  logger.info("Running drizzle migrations...");
   await migrate(db, { migrationsFolder: "./drizzle" });
   await pool.end();
-  console.log("Migrations applied");
+  logger.info("Migrations applied");
 }
 
 main().catch((err) => {
-  console.error(err);
+  logger.error({ err }, "Migration failed");
   process.exit(1);
 });
