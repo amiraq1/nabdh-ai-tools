8578d7491e29808489d45521b86f948b7aa0b59c
amiraq1 <amaralmdarking27@gmail.com>
2025-12-13 00:23:21 +0300

    Add pg module declarations for Pool and PoolClient

--- server/types/pg.d.ts
+++ server/types/pg.d.ts
@@ -1,12 +1,23 @@
 // server/types.d.ts
 export {};
 
-declare global {
-  namespace Express {
-    interface User {
-      id: string;
-      email?: string | null;
-      role?: string;
-    }
+declare module "pg" {
+  export interface PoolConfig {
+    connectionString?: string;
+    ssl?: unknown;
+    max?: number;
+    idleTimeoutMillis?: number;
+    connectionTimeoutMillis?: number;
+  }
+
+  export class Pool {
+    constructor(config?: PoolConfig);
+    connect(): Promise<PoolClient>;
+    end(): Promise<void>;
+    query: (...args: unknown[]) => Promise<unknown>;
+  }
+
+  export interface PoolClient {
+    release?: () => void;
   }
 }
