3a9e569032458de742def4088a993a124869cc61
amiraq1 <amaralmdarking27@gmail.com>
2025-12-13 00:24:34 +0300

    Refactor pg.d.ts to extend Express User interface
    Removed Pool and PoolClient declarations and added User interface to Express namespace.

--- server/types/pg.d.ts
+++ server/types/pg.d.ts
@@ -1,23 +1,12 @@
 // server/types.d.ts
 export {};
 
-declare module "pg" {
-  export interface PoolConfig {
-    connectionString?: string;
-    ssl?: unknown;
-    max?: number;
-    idleTimeoutMillis?: number;
-    connectionTimeoutMillis?: number;
-  }
-
-  export class Pool {
-    constructor(config?: PoolConfig);
-    connect(): Promise<PoolClient>;
-    end(): Promise<void>;
-    query: (...args: unknown[]) => Promise<unknown>;
-  }
-
-  export interface PoolClient {
-    release?: () => void;
+declare global {
+  namespace Express {
+    interface User {
+      id: string;
+      email?: string | null;
+      role?: string;
+    }
   }
 }
